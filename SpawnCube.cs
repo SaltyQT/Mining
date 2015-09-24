8224c749fad39487be5a17452c730df8d12a7867
farseeraslan <tcb14@hampshire.edu>
2015-09-24 17:50:57 -0400

    Part 2
    Spawn Cube code

---
+++ SpawnCube.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnCube : MonoBehaviour {
+
+	public GameObject Cube;
+	bool cubeSpawned = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeToSpawnCube()) {
+			SpawnTheCube();
+		}
+	}
+
+	void SpawnTheCube() {
+		Vector3 cubePosition = new Vector3(0.0f, 0.0f, 0.0f);
+		Instantiate (Cube, cubePosition, Quaternion.identity);
+		cubeSpawned = true;
+	}
+
+	bool timeToSpawnCube(){
+		if (Time.time >= 3.0f &&! cubeSpawned) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
